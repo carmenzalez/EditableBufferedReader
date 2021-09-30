175c9aa94331eea7bc2282031b9a8398e351c85b
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-09-30 19:57:01 +0200

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,46 @@
+package LAB1;
+
+/**
+ *
+ * @author pau
+ */
+public class Line {
+    
+    public String line;
+    
+    public Line() {
+        line = "";
+    }
+    
+    public String getLine() {
+        return line;
+    }
+    
+    public void addChar(int c) {
+        line = line + (char) c;
+    }
+    
+    public void left() {
+        
+    }
+    
+    public void right() {
+        
+    }
+    
+    public void home() {
+        
+    }
+    
+    public void end() {
+        
+    }
+    
+    public void insert() {
+        
+    }
+    
+    public void delete() {
+        
+    }
+}
