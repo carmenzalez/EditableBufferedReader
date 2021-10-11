050fa28e8ed4454fe9b553e4db643c4094d62004
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-10-11 12:57:17 +0200

    Add files via upload

--- Line.java
+++ Line.java
@@ -19,34 +19,46 @@ public class Line {
     }
     
     public String getLine() {
-        pos = line.length();
+        //pos = line.length();
+        end();
         return line.toString();
     }
     
     public void addChar(int c) {
+        if (len != pos) {
+            //System.out.print(line.substring(pos, len).toString());
+            //System.out.print("\033["+ (len - pos) +"D");
+            System.out.append((char) c);
+        }
         line.insert(pos, (char) c);
+        len = line.length();
         pos++;
     }
     
     public void left() {
         if (pos > 0) {
             pos--;
+            System.out.print("\033[1D");
         }
-        System.out.print("\b");
     }
     
     public void right() {
         if (pos < line.length()) {
             pos++;
+            System.out.print("\033[1C");
         }
     }
     
     public void home() {
+        System.out.print("\033["+ pos +"D");
         pos = 0;
     }
     
     public void end() {
-        pos = line.length();
+        if (pos != len) {
+            System.out.print("\033["+ (len - pos) +"C");
+            pos = line.length();
+        }
     }
     
     public void insert() {
@@ -54,10 +66,14 @@ public class Line {
     }
     
     public void delete() {
-        if (pos < line.length()) {
+        if (pos <= len) {
             line.deleteCharAt(pos);
             //line.substring(pos);
+            //System.out.print("\033[1D");
             len = line.length() -1;
+            System.out.print(line.substring(pos,len).toString());
+            //System.out.print("\033[1D");
+            //System.out.print(line.substring(pos,len).toString());
         }
     }
 }
