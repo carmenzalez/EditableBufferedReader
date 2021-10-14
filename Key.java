5f09d8f77ea575b7c72387cd20aebc61fe2ef66e
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-10-14 18:41:02 +0200

    Add files via upload

--- Key.java
+++ Key.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package EditableBR;
+package LAB1;
 
 /**
  *
- * @author carmeeeen
+ * @author pau
  */
 public class Key {
     
-    static final int ESC = 27;
+    /*static final int ESC = 27;
     static final int RIGHT = 67;
     static final int LEFT = 68;
     static final int HOME = 72;
@@ -20,16 +15,16 @@ public class Key {
     static final int DELETE = 51;
     static final int LBRACKET = 91;
     static final int TILDE = 126;
-    static final int ENTER = 13;
+    static final int ENTER = 13;*/
 
-    static final int R_ESC = 1000;
-    static final int R_RIGHT = 1001;
-    static final int R_LEFT = 1002;
-    static final int R_HOME = 1003;
-    static final int R_END = 1004;
-    static final int R_INSERT = 1005;
-    static final int R_DELETE = 1006;
-    static final int R_LBRACKET = 1007;
-    static final int R_BACKSPACE = 1008;
+    static final int ESC = 27;
+    static final int RIGHT = -5;
+    static final int LEFT = -6;
+    static final int HOME = -4;
+    static final int END = -1;
+    static final int INSERT = -3;
+    static final int DELETE = -2;
+    static final int BACKSPACE = 127;
+    static final int ENTER = 13;
 
 }
