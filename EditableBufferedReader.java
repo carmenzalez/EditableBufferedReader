fb953ccb61d83ee164f7c9d3330ab7bfd2046571
Carmen González <90858558+carmenzalez@users.noreply.github.com>
2021-10-14 12:56:50 +0200

    Add files via upload

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,44 +1,22 @@
-package LAB1;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+package EditableBR;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+
 /**
  *
- * @author pau
+ * @author carmeeeen
  */
 public class EditableBufferedReader extends BufferedReader {
-
-    static final int ESC = 27;
-    static final int RIGHT = 67;
-    static final int LEFT = 68;
-    static final int HOME = 72;
-    static final int END = 70;
-    static final int INSERT = 50;
-    static final int DELETE = 51;
-    static final int LBRACKET = 91;
-    static final int TILDE = 126;
-    static final int ENTER = 13;
-
-    static final int R_ESC = 1000;
-    static final int R_RIGHT = 1001;
-    static final int R_LEFT = 1002;
-    static final int R_HOME = 1003;
-    static final int R_END = 1004;
-    static final int R_INSERT = 1005;
-    static final int R_DELETE = 1006;
-    static final int R_LBRACKET = 1007;
-    static final int R_BACKSPACE = 1008;
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public EditableBufferedReader(Reader in) {
         super(in);
     }
@@ -71,16 +49,16 @@ public class EditableBufferedReader extends BufferedReader {
             case '[':
                 switch (c = super.read()) {
                     case 'C':
-                        return R_RIGHT;
+                        return Key.R_RIGHT;
                     case 'D':
-                        return R_LEFT;
+                        return Key.R_LEFT;
                     case 'H':
-                        return R_HOME;
+                        return Key.R_HOME;
                     case 'F':
-                        return R_END;
+                        return Key.R_END;
                     case '3':
                         super.read();
-                        return R_DELETE;
+                        return Key.R_DELETE;
                     /*case '1':
                         if((ch1 = super.read()) != '~')
                             return ch1;
@@ -112,27 +90,27 @@ public class EditableBufferedReader extends BufferedReader {
         do {
             c = this.read();
             switch (c) {
-                case R_RIGHT:
+                case Key.R_RIGHT:
                     line.right();
                     break;
 
-                case R_LEFT:
+                case Key.R_LEFT:
                     line.left();
                     break;
 
-                case R_HOME:
+                case Key.R_HOME:
                     line.home();
                     break;
 
-                case R_END:
+                case Key.R_END:
                     line.end();
                     break;
 
-                case R_INSERT:
+                case Key.R_INSERT:
                     line.insert();
                     break;
 
-                case R_DELETE:
+                case Key.R_DELETE:
                     line.delete();
                     System.out.print("\033[K");
                     break;
@@ -142,7 +120,7 @@ public class EditableBufferedReader extends BufferedReader {
                     System.out.print("\033[1K");
                     break;
 
-                case ENTER:
+                case Key.ENTER:
                     break;
 
                 default:
@@ -151,15 +129,11 @@ public class EditableBufferedReader extends BufferedReader {
             System.out.print("\r" + line.getLine());
             //System.out.print(line.getIndex() + 1); //Visualitzador de posició en temps real
             System.out.print("\033[" + (line.getIndex() + 1) + "G");
-        } while (c != ENTER);
+            
+        } while (c != Key.ENTER);
         unsetRaw();
         return line.getLine();
     }
 
-    /*public static void main(String[] args) throws IOException {
-        int c;
-        EditableBufferedReader e = new EditableBufferedReader(new InputStreamReader(System.in));
-        c = e.read();
-        System.out.println(c);
-    }*/
+    
 }
