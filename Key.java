08b36897971499cc5d69d9f7710205058b55d7aa
Carmen González <90858558+carmenzalez@users.noreply.github.com>
2021-10-14 12:53:40 +0200

    Add files via upload

---
+++ Key.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EditableBR;
+
+/**
+ *
+ * @author carmeeeen
+ */
+public class Key {
+    
+    static final int ESC = 27;
+    static final int RIGHT = 67;
+    static final int LEFT = 68;
+    static final int HOME = 72;
+    static final int END = 70;
+    static final int INSERT = 50;
+    static final int DELETE = 51;
+    static final int LBRACKET = 91;
+    static final int TILDE = 126;
+    static final int ENTER = 13;
+
+    static final int R_ESC = 1000;
+    static final int R_RIGHT = 1001;
+    static final int R_LEFT = 1002;
+    static final int R_HOME = 1003;
+    static final int R_END = 1004;
+    static final int R_INSERT = 1005;
+    static final int R_DELETE = 1006;
+    static final int R_LBRACKET = 1007;
+    static final int R_BACKSPACE = 1008;
+
+}
